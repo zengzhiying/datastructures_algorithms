4590effd13fbc98fc855570ca6f0747fc368f807
zengzhiying <yingzhi_zeng@126.com>
2025-02-19 20:18:39 +0800

    完善牛顿插值法代码以动态方式自动生成输入样本

--- math/newton-interpolation/main.c
+++ math/newton-interpolation/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 // 计算差商表
-void divided_differences(double x[], double f[][18], int n) {
-    // 计算一阶及更高阶差商
+void divided_differences(double x[], double **f, int n) {
     for (int i = 1; i < n; i++) {
         for (int j = i; j < n; j++) {
             f[i][j] = (f[i - 1][j] - f[i - 1][j - 1]) / (x[j] - x[j - i]);
@@ -11,7 +12,7 @@ void divided_differences(double x[], double f[][18], int n) {
 }
 
 // 牛顿插值法计算函数值
-double newton_interpolation(double x[], double f[][18], int n, double X) {
+double newton_interpolation(double x[], double **f, int n, double X) {
     double result = f[0][0];
     double term = 1.0;
     // 根据差商表计算插值结果
@@ -23,19 +24,24 @@ double newton_interpolation(double x[], double f[][18], int n, double X) {
 }
 
 int main() {
-    double f[18][18];
     int n = 9;
+    double **f = (double **) malloc(n * sizeof(double *));
+    double *x;
+    for(int i = 0; i < n; i++) {
+        f[i] = (double *) malloc(n * sizeof(double));
+        x = (double *) malloc(n * sizeof(double));
+    }
 
     // 待估计自变量
+    // x: 0.32, y: 0.9492
     double X = 0.32;
 
-    double x[9] = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9};
-    
-    double y[9] = {0.9950041652780258, 0.9800665778412416, 0.955336489125606, 0.9210609940028851, 0.8775825618903728, 0.8253356149096783, 0.7648421872844885, 0.6967067093471654, 0.6216099682706644};
-
-    // x: 0.32, y: 0.9492
+    // cos 函数测试
+    double x0 = 0.1;
     for(int i = 0; i < n; i++) {
-        f[0][i] = y[i];
+        x[i] = x0;
+        f[0][i] = cos(x[i]);
+        x0 += 0.1;
     }
 
     // 计算差商表
@@ -45,6 +51,12 @@ int main() {
 
     // 输出结果
     printf("根据牛顿插值法得到的函数估计值为：f(%.9lf) = %.9lf\n", X, estimated_value);
+
+    free(x);
+    for(int i = 0; i < n; i++) {
+        free(f[i]);
+    }
+    free(f);
 
     return 0;
 }
